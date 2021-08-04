3f35d0c015ba92cfb3e77c8948eea1806fa6a728
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-04 15:53:03 -0400

    tags

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -3,7 +3,7 @@ const {Product} = require('../models');
 const productdata = [
 
 {
-    product_name: 'Plain t-shirt',
+    product_name: 'MLB logo t-shirt',
     price: 9.99,
     stock: 20,
     category_id: 1, 
@@ -12,26 +12,30 @@ const productdata = [
     product_name: 'Cleated Baseball Shoes',
     price: 75.00,
     stock: 25,
-    category_id: 5,
+    category_id: 2,
     },
 {
     product_name: 'MLB Hat',
     price: 29.99,
     stock: 30,
-    category_id: 4,
+    category_id: 3,
     },
 {
     product_name: 'Replica MLB Baseball Gloves',
     price: 99.99,
     stock: 15,
-    category_id: 3,
+    category_id: 4,
     },
-];
+{   
+        product_name: 'MLB Baseball Hat',
+    price: 22.99,
+    stock: 20,
+    category_id: 4,
+},
+]
 
-
-
-
-
+const seedProducts = () => Product.bulkCreate(productdata);
+module.exports = seedProducts;
 
 
 
---
+++ seeds/tag-seeds.js
@@ -0,0 +1,27 @@
+const {Tag} = require('../models');
+const tagdata = [
+{
+    tag_name: 'Olympic events',
+},
+{
+    tag_name: 'Track & field',
+},
+{   
+    tag_name: 'Basketball'
+},
+{
+    tag_name: 'Gymnastics',
+},
+{
+    tag_name: 'Equestrian',
+},
+{
+    tag_name: 'Fencing',
+},
+{
+    tag_name: 'badminton',
+},
+{
+    tag_name:'wrestling',
+
+}]
