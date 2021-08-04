2e539ec347b2d000f09f8c4c08f442e012f7c06a
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-04 11:34:09 -0400

    product seeds

---
+++ seeds/product-seeds.js
@@ -0,0 +1,39 @@
+const {Product} = require('../models');
+
+const productdata = [
+
+{
+    product_name: 'Plain t-shirt',
+    price: 9.99,
+    stock: 20,
+    category_id: 1, 
+},
+    {
+    product_name: 'Cleated Baseball Shoes',
+    price: 75.00,
+    stock: 25,
+    category_id: 5,
+    },
+{
+    product_name: 'MLB Hat',
+    price: 29.99,
+    stock: 30,
+    category_id: 4,
+    },
+{
+    product_name: 'Replica MLB Baseball Gloves',
+    price: 99.99,
+    stock: 15,
+    category_id: 3,
+    },
+];
+
+
+
+
+
+
+
+
+
+
