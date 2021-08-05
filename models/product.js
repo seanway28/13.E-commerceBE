0335eb48c0065419211b4021de5f29c7c3097f88
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-05 11:32:21 -0400

    product

---
+++ models/product.js
@@ -0,0 +1,62 @@
+// import important parts of sequelize library
+
+const { Model, DataTypes } = require('sequelize');
+
+//import our database connection from config.js
+
+const sequelize=request(',,/config/connection');
+
+//initialize Product model (table) by extending off sequalizer model class
+
+class product extends Model{}
+
+//set up fields amd rules for Product Model
+
+Product.init (
+    {
+        //define columns
+        id:{
+            type:DataTypes.INTEGER
+            ,allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true,
+
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+          },
+          
+          price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+            validate: {
+              isDecimal: true
+            }
+        },
+        stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+            isNumeric: true
+          }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                mode: 'category',
+                key: 'id'
+}
+    }},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
+
