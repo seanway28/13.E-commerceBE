b47faa4692135757d1ef8ed7163886322d20c48b
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-06 12:50:07 -0400

    category routes cont.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,3 +29,55 @@ Product,  then(dbCategoryData => res.json(dbCategoryData))
 
    });
 });
+
+router.put('/:id', (req, res) => {
+//update a category by its 'id' values
+Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(400).json({ message: 'No category with that id found'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
+.then(dbCategoryData => {
+if (!dbCategoryData){
+    res.status(400),json({ message:'No categorywith this id found'});
+    return;
+
+}
+
+res.json(dbCategoryData);
+})
+
+.then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(400).json({ message: 'No category with that id found'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
